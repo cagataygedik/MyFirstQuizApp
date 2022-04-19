2b2429968636179236b0b56a03416de7a99380fc
cagataygedik <cagatay.gedik@icloud.com>
2022-04-19 13:55:59 +0300

    My First Commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What is your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Mavi', 'score': 5},
+        {'text': 'Yeşil', 'score': 1},
+        {'text': 'Red', 'score': 3},
+      ],
+    },
+    {
+      'questionText': 'What is your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Dog', 'score': 1},
+        {'text': 'Cat', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'What is your favorite tv show?',
+      'answers': [
+        {'text': 'Breaking Bad', 'score': 5},
+        {'text': 'Friends', 'score': 1},
+        {'text': 'Black Mirror', 'score': 3},
+        {'text': 'Sherlock', 'score': 2},
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('You finished all the questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MyFirstQuizApp'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
